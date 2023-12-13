464a9a7fdd9f11f842b534b91e08363dc514ee55
DeyvisonTav <deyvisontav@gmail.com>
2023-12-12 22:57:37 -0300

    feat: added layout in home-page

---
+++ src/components/ui/button-home.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { ComponentProps } from "react";
+import { useRouter } from "next/navigation";
+
+interface ButtonProps extends ComponentProps<"button"> {}
+export function Button({ children }: ButtonProps) {
+  const router = useRouter();
+  function handleClick() {
+    router.push("/#home");
+  }
+  return (
+    <button
+      onClick={handleClick}
+      className="bg-primary lg:px-28 px-10 py-1 lg:py-4 rounded-full hover:bg-opacity-80
+      transition-colors duration-300"
+    >
+      {children}
+    </button>
+  );
+}
+
+export function ButtonText({ children }: ButtonProps) {
+  return (
+    <span className="text-secondary lg:text-xl text-sm font-bold">
+      {children}
+    </span>
+  );
+}
