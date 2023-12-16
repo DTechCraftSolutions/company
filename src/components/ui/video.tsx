e29fabb9d8c16aedff5546ef73baaac239ccf345
DeyvisonTav <deyvisontav@gmail.com>
2023-12-16 13:05:00 -0300

    fix: ajusment in test e2e

--- src/components/ui/video.tsx
+++ src/components/ui/video.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function VideoComponent() {
   return (
-    <div className="w-[100%] lg:h-[100vh] h-[56vh] bg-home bg-no-repeat bg-cover absolute -z-[1000]">
+    <div className="w-[100%] lg:h-[100vh] h-[56vh] md:h-[80vh] bg-home bg-no-repeat bg-cover absolute -z-[1000]">
       {/* 
       refator for the feature
       <video autoPlay muted loop src="/video-company.mp4" />
